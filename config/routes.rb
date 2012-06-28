ff575758ee834b88242ff88ccfccf46837a11eef
Chris Hallendy <chris.hallendy@gmail.com>
2012-06-28 17:04:24 -0400

    adding User scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 ItInTheD::Application.routes.draw do
+  resources :users
+
+  # match '/person/:id' => 'users#show'
+
+  # match '/people' => 'users#index'
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => 'users#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
