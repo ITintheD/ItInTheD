bcb7bdaa376f9fc00a3d7311e4098bd3fc28b0da
Timothy Fisher <timothyf@gmail.com>
2012-07-10 15:48:28 -0400

    new home page files

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ ItInTheD::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'users#index'
+  root :to => 'home#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -66,5 +66,5 @@ ItInTheD::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
 end
