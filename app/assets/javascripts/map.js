2ee60fe83727b9ce81d4713fdae61399ddda57f5
Timothy Fisher <timothyf@gmail.com>
2012-07-17 13:27:03 -0400

    map changes

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,10 +1,21 @@
 
 function initialize() {
-    var myOptions = {
+	var myOptions = {
       center: new google.maps.LatLng(42.352931,-83.090286),
       zoom: 12,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),
-        myOptions);
+
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+	addCompanies(map);
+}
+
+
+function addCompanies(map) {
+  var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(42.332566,-83.046856),
+      map: map,
+      title:"Compuware"
+  });
 }
