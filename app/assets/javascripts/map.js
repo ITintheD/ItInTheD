5dc8472400a6f46966820eeb5387551007cca52e
Timothy Fisher <timothyf@gmail.com>
2012-07-17 13:41:45 -0400

    map changes

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,10 +3,47 @@ function initialize() {
 	var myOptions = {
       center: new google.maps.LatLng(42.352931,-83.090286),
       zoom: 12,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+	  styles:[
+	  {
+	    featureType: "landscape",
+	    elementType: "geometry.fill",
+	    stylers: [
+	      { visibility: "on" },
+	      { weight: 3.6 },
+	      { saturation: 100 },
+	      { hue: "#0077ff" }
+	    ]
+	  },{
+	    featureType: "water",
+	    elementType: "geometry.fill",
+	    stylers: [
+	      { saturation: 86 },
+	      { hue: "#00b2ff" }
+	    ]
+	  },{
+	    featureType: "road",
+	    elementType: "geometry.fill",
+	    stylers: [
+	      { hue: "#0077ff" }
+	    ]
+	  }	,{
+		    featureType: "road",
+		    elementType: "labels.text.stroke",
+		    stylers: [
+		      { hue: "#00f6ff" },
+		      { saturation: 6 },
+		      { lightness: 30 },
+		      { gamma: 1.56 }
+		    ]
+		  }
+	]
     };
 
-    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    var map = new google.maps.Map(
+		document.getElementById("map_canvas"), 
+		myOptions
+	);
 
 	addCompanies(map);
 }
