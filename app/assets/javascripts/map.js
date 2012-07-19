90208c6e3521ed0b260c983141cafa67c16b42ae
Cecilina <cecilina850130@gmail.com>
2012-07-18 20:10:04 -0700

    adding a marker for schools and infowindow for events

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,7 +2,7 @@
 function initialize() {
 	var myOptions = {
       center: new google.maps.LatLng(42.352931,-83.090286),
-      zoom: 12,
+      zoom: 11,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
 	  styles:[
 	  {
@@ -47,6 +47,7 @@ function initialize() {
 
 	addCompanies(map);
 	addEvents(map);
+	addUniversities(map);
 }
 
 
@@ -56,8 +57,43 @@ function addCompanies(map) {
       map: map,
       title:"Compuware"
   });
+  var marker = new google.maps.Marker({
+  	position: new google.maps.LatLng (42.338753, -83.048115),
+  	map: map,
+  	title: "Quicken Loans"
+  });
 }
 
 function addEvents(map) {
+	var event ="Mobile Monday Detroit";
+	var infowindow = new google.maps.InfoWindow({
+		content: event,
+		size: new google.maps.Size(50,50)
+	});
+	var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(42.332566,-83.046856),
+      map: map,
+      title:"Compuware"
+  });
+	google.maps.event.addListener(marker,'click',function(){
+		infowindow.open(map,marker);
+	})
+  
 	
+}
+function addUniversities(map){
+	var image = '/assets/yellow-dot.png';
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(42.297373, -83.109884),
+		map: map,
+		icon: image,
+		title: "Wayne State University"
+});
+		
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(42.3352, -83.053265),
+		map: map,
+		icon: image,
+		title: "Wayne County Community College"
+	});
 }
