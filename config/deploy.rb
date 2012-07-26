c0ca5ae42875f9ec59a217ae43feaf722d871f3e
Chris Hallendy <chris.hallendy@gmail.com>
2012-07-26 17:07:00 -0400

    updates to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,25 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "ItInTheD"
+set :repository,  "git@github.com:Compuware/ItInTheD.git"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "ec2-50-16-38-11.compute-1.amazonaws.com"                          # Your HTTP server, Apache/etc
+role :app, "ec2-50-16-38-11.compute-1.amazonaws.com"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+set :deploy_to, "/home/ubuntu/"
+
+set :user, "ubuntu"
+default_run_options[:pty] = true
+ssh_options[:keys] = ["#{ENV['HOME']}/Desktop/ItInTheD.pem"]
+
+# set :deploy_via, :remote_cache
+# set :repository_cache, "copy_of_your_checkout_instance"
+# set :keep_releases, 3
+
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
