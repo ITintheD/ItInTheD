77bb7b58e03a75d8c2c23abdf25350c66072f3ed
Eyad Hailat <eyad@wayne.edu>
2012-08-02 17:53:40 -0400

    fixed deploy.er that had one typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
-  task :migrations do
+  task :migration do
   run "rake db:migrate"
   end
 end
