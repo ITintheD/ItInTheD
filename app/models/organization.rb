f8a48eea41a4588400beb229304be5e189404e00
Stirling Larson <stirlingdavidlarson@gmail.com>
2012-08-07 16:25:33 -0400

    Edited org model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,7 +2,7 @@ class Organization < ActiveRecord::Base
 
   attr_accessible :address, :city, :lat, :long, :name, :org_url, :web_link, :zip, :keyword_list, :org_type_id, :logo, :org_num_students_employ, :org_start_year, :org_annual_sales, :org_optional_stat_a, :org_job, :org_phone, :org_state
 
-  has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  #has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   acts_as_taggable
   acts_as_taggable_on :keywords
   belongs_to :org_type
