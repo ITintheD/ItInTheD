9ec74ddbc9fc2c71949c749361e08a143d338b8e
Cecilina <cecilina850130@gmail.com>
2012-08-16 13:12:08 +0800

    modify user and event form and try to add event list to user profile

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ActiveRecord::Base
   attr_accessible :local, :time, :title
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users, #:dependent => :destroy
+  #validates :title, :presence => true, :length => { :maximum => 50 }
+  #validates :user_id, :presence => true
+
+  #default_scope :order => 'events.created_at DESC'
 end
