39f670ac1601dd9000b4cd60dbb6057a0fb5f2dd
Cecilina <cecilina850130@gmail.com>
2012-08-22 05:21:16 +0800

    add icon for user's resume

--- db/schema.rb
+++ db/schema.rb
@@ -54,14 +54,13 @@ ActiveRecord::Schema.define(:version => 20120821194517) do
     t.string   "org_optional_stat_a"
     t.string   "org_optional_stat_b"
     t.string   "org_optional_stat_c"
-    t.string   "logo"
-    t.string   "org_job"
-    t.string   "org_phone"
-    t.string   "org_state"
     t.string   "logo_file_name"
     t.string   "logo_content_type"
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
+    t.string   "org_job"
+    t.string   "org_phone"
+    t.string   "org_state"
   end
 
   create_table "taggings", :force => true do |t|
@@ -110,7 +109,6 @@ ActiveRecord::Schema.define(:version => 20120821194517) do
     t.string   "city"
     t.string   "state"
     t.string   "zip"
-    t.string   "resume"
     t.string   "resume_file_name"
     t.string   "resume_content_type"
     t.integer  "resume_file_size"
