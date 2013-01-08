35354f50c0744adbb5a5b097cf99a8043665e126
Andrea Jenkins <andrea.r.jenkins@gmail.com>
2013-01-08 16:31:09 -0500

    fixed user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @event = Event.find(params[:id])#.paginate(:page => params[:page])
+    #@event = Event.find(params[:id])#.paginate(:page => params[:page])
 
     respond_to do |format|
       format.html # show.html.erb
