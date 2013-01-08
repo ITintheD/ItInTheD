92cc7112795ff44aa5f3c34dc3824c4c5eb34d3f
Eric K. Howe <smalltalk91@gmail.com>
2013-01-08 17:18:43 -0500

    fixed organization urls

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121230042337) do
+ActiveRecord::Schema.define(:version => 20130108215135) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(:version => 20121230042337) do
     t.integer  "resource_count"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+    t.string   "resource_type"
   end
 
   create_table "socials", :force => true do |t|
