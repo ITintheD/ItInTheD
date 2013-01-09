0b75fe3db947732d19c9fedc6b3ea1e854ecb70d
Johnnie D. Harris <jdharris@comcast.net>
2013-01-09 17:56:17 -0500

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(:version => 20121230042337) do
     t.integer  "resource_count"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
-    t.string   "resource_type"
   end
 
   create_table "socials", :force => true do |t|
@@ -137,7 +136,6 @@ ActiveRecord::Schema.define(:version => 20121230042337) do
     t.integer  "resume_file_size"
     t.datetime "resume_updated_at"
     t.string   "telephone"
-    t.string   "user_type"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
