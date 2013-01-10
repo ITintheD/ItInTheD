dbf63ce6314d61502c32a06a4f2ee9c9f6951aac
mlou <mlou.mohn@gmail.com>
2013-01-10 08:37:36 -0500

    MLM added in new faq and contact page layouts

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ ItInTheD::Application.routes.draw do
   devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout'}
 
 
-  resources :contactus
+  resources :contact_us
 
   # match '/person/:id' => 'users#show'
   
