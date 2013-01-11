bfa9169d0ec7a6f963cb2cb8886448971b49a254
mlou <mlou.mohn@gmail.com>
2013-01-11 14:08:24 -0500

    start of community page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130109191701) do
+ActiveRecord::Schema.define(:version => 20130110123553) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -100,6 +100,12 @@ ActiveRecord::Schema.define(:version => 20130109191701) do
 
   create_table "tags", :force => true do |t|
     t.string "name"
+  end
+
+  create_table "testimonials", :force => true do |t|
+    t.string   "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
